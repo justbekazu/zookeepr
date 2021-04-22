bb25ca4c64546aea417ea2960b21803e1df67b1f
djkaz <justbekazu@gmail.com>
2021-04-21 22:13:09 -0500

    fixed animals

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require('express');
 //to instantiate the server
 const app = express();
 //requiring data from the animial.json file
-const { animals } = require('./data/animal.json');
+const { animal } = require('./data/animal.json');
 
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-  });
+//this is the get function that tells the client where to get the info and then what will happen once the info is gotten
+ app.get('/api/animal', (req, res) => {
+ res.send('Hello!');
+ });
 
 //make our server listen for requests
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+console.log(`API server now on port 3001!`);
+});
 

1fbe333a842a6292ba5473efeba493378042d737
djkaz <justbekazu@gmail.com>
2021-04-21 23:25:01 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const express = require('express');
 //to instantiate the server
 const app = express();
 //requiring data from the animial.json file
-const { animal } = require('./data/animal.json');
+const { animals } = require('./data/animals.json');
+
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
 
 //this is the get function that tells the client where to get the info and then what will happen once the info is gotten
- app.get('/api/animal', (req, res) => {
- res.send('Hello!');
- });
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
 
 //make our server listen for requests
 app.listen(3001, () => {

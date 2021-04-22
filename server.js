b418224945a515a85b9227c1db51a3ae76ff9a1b
djkaz <justbekazu@gmail.com>
2021-04-21 21:50:02 -0500

    added access to the json file

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require('express');
 //to instantiate the server
 const app = express();
+//requiring data from the animial.json file
+const { animals } = require('./data/animal.json');
+
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+  });
 
 //make our server listen for requests
 app.listen(3001, () => {
